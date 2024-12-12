b43604555bbb86746b65b112b1133855878c0091
GabyGuC <gaby.98uc@gmail.com>
2024-12-11 20:01:45 -0600

    Se añaden requerimientos de bases de datos

---
+++ DB_requeriments/insercionesBD.sql
@@ -0,0 +1,5 @@
+insert into consola (console_id, name) values (1, 'Xbox');
+insert into consola (console_id, name) values (2, 'PlayStation');
+insert into consola (console_id, name) values (3, 'pc');
+insert into consola (console_id, name) values (4, 'all');
+insert into consola (console_id, name) values (5, 'mobile');
