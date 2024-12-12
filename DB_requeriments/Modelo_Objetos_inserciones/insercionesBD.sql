fce52f6c33ac66ab5005b65392980ae1bc40f1d0
GabyGuC <gaby.98uc@gmail.com>
2024-12-11 20:59:36 -0600

    se añaden objetos e inserts para base de datos

---
+++ DB_requeriments/Modelo_Objetos_inserciones/insercionesBD.sql
@@ -0,0 +1,55 @@
+
+insert into consola (console_id, name) values (1, 'Xbox');
+insert into consola (console_id, name) values (2, 'PlayStation');
+insert into consola (console_id, name) values (3, 'pc');
+insert into consola (console_id, name) values (4, 'all');
+insert into consola (console_id, name) values (5, 'mobile');
+insert into consola (console_id, name) values (6, 'nintendo');
+
+
+
+insert into videogame (videogame_id, name, price,is_discount,totalsold,console_id) values (1, 'Super Mario Party',900,false,50,6);
+insert into videogame (videogame_id, name, price,is_discount,totalsold,console_id) values (2, 'Super Smash Bros. Ultimate',1000,false,200,6);
+insert into videogame (videogame_id, name, price,is_discount,totalsold,console_id) values (3, 'Super Mario Bros. Wonder',1000,false,80,6);
+insert into videogame (videogame_id, name, price,is_discount,totalsold,console_id) values (4, 'The Legend of Zelda: Tears of the Kingdom',1500,false,190,6);
+insert into videogame (videogame_id, name, price,is_discount,totalsold,console_id) values (5, 'Forza horizon 5',800,false,74,1);
+insert into videogame (videogame_id, name, price,is_discount,totalsold,console_id) values (6, 'Assassins creed origins',750,false,65,4);
+insert into videogame (videogame_id, name, price,is_discount,totalsold,console_id) values (7, 'Assassins creed odisey',700,false,33,4);
+insert into videogame (videogame_id, name, price,is_discount,totalsold,console_id) values (8, 'Assassins creed valhalla',850,false,38,4);
+insert into videogame (videogame_id, name, price,is_discount,totalsold,console_id) values (9, 'GTA 5',1000,false,100,2);
+insert into videogame (videogame_id, name, price,is_discount,totalsold,console_id) values (10, 'The last of us',800,false,90,2);
+insert into videogame (videogame_id, name, price,is_discount,totalsold,console_id) values (11, 'Good of war',500,false,79,2);
+insert into videogame (videogame_id, name, price,is_discount,totalsold,console_id) values (12, 'Halo',1000,false,90,1);
+insert into videogame (videogame_id, name, price,is_discount,totalsold,console_id) values (13, 'Residen evil 4',1000,true,100,4);
+insert into videogame (videogame_id, name, price,is_discount,totalsold,console_id) values (14, 'Geometry dash pc',80,false,236,3);
+insert into videogame (videogame_id, name, price,is_discount,totalsold,console_id) values (15, 'COD black mobile',80,false,450,5);
+insert into videogame (videogame_id, name, price,is_discount,totalsold,console_id) values (16, 'The last of us',800,false,90,2);
+
+
+insert into reviews (reviews_id, review,score,videogame_id) values (1, 'Ist de best',5,12);
+insert into reviews (reviews_id, review,score,videogame_id) values (2, 'I really enjoy this game',5,12);
+insert into reviews (reviews_id, review,score,videogame_id) values (3, 'My favorite game in the world',5,12);
+insert into reviews (reviews_id, review,score,videogame_id) values (4, 'Is the best but expensive',4,4);
+insert into reviews (reviews_id, review,score,videogame_id) values (5, 'I really enjoy this game',5,4);
+insert into reviews (reviews_id, review,score,videogame_id) values (6, 'Is the best x2',5,4);
+insert into reviews (reviews_id, review,score,videogame_id) values (7, 'Is too short',2,14);
+insert into reviews (reviews_id, review,score,videogame_id) values (8, 'When launch the part 3???',4,8);
+insert into reviews (reviews_id, review,score,videogame_id) values (9, 'I expected more',1,15);
+insert into reviews (reviews_id, review,score,videogame_id) values (10, 'I really enjoy this game but is expensive',3,3);
+insert into reviews (reviews_id, review,score,videogame_id) values (11, 'My favorite game in the world',4,1);
+insert into reviews (reviews_id, review,score,videogame_id) values (12, 'Is not de best',2,2);
+insert into reviews (reviews_id, review,score,videogame_id) values (13, 'I really enjoy this game',5,5);
+insert into reviews (reviews_id, review,score,videogame_id) values (14, 'My favorite game in the world',4,6);
+insert into reviews (reviews_id, review,score,videogame_id) values (15, 'I really enjoy this game',5,7);
+insert into reviews (reviews_id, review,score,videogame_id) values (16, 'My favorite game in the world',4,8);
+insert into reviews (reviews_id, review,score,videogame_id) values (17, 'Ist de best',5,12);
+insert into reviews (reviews_id, review,score,videogame_id) values (18, 'I really enjoy this game',5,9);
+insert into reviews (reviews_id, review,score,videogame_id) values (19, 'My favorite game in the world',4,10);
+insert into reviews (reviews_id, review,score,videogame_id) values (20, 'I really enjoy this game',5,12);
+insert into reviews (reviews_id, review,score,videogame_id) values (21, 'My favorite game in the world',5,11);
+insert into reviews (reviews_id, review,score,videogame_id) values (22, 'Ist de best',5,13);
+insert into reviews (reviews_id, review,score,videogame_id) values (23, 'I really enjoy this game',4,16);
+insert into reviews (reviews_id, review,score,videogame_id) values (24, 'My favorite game in the world',5,16);
+insert into reviews (reviews_id, review,score,videogame_id) values (25, 'My favorite game',4,14);
+
+
--- DB_requeriments/insercionesBD.sql
+++
@@ -1,5 +0,0 @@
-insert into consola (console_id, name) values (1, 'Xbox');
-insert into consola (console_id, name) values (2, 'PlayStation');
-insert into consola (console_id, name) values (3, 'pc');
-insert into consola (console_id, name) values (4, 'all');
-insert into consola (console_id, name) values (5, 'mobile');
